5a1575482947f555e7c08ce66dbb3bf17c22317d
travishbates <travishbates@gmail.com>
2023-09-28 01:33:19 -0400

    Add AppBar.

--- gains-tracker-ui/src/App.tsx
+++ gains-tracker-ui/src/App.tsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
 import { Container } from '@mui/material'
 import './App.css'
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
   return (
-    <Container>
-      <h1>Gains Tracker</h1>
-    </Container>
+    <>
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton color="inherit">
+                    <MenuIcon />
+                </IconButton>
+
+                <Typography variant="h5" sx={{ flexGrow: 1 }}>Gains Tracker</Typography>
+
+                <Button color="inherit">Login</Button>
+            </Toolbar>
+        </AppBar>
+        <Container>
+            Welcome to Gains Tracker!
+        </Container>
+    </>
   )
 }
 
