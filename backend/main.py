46e3d83d19571947495b4199d1f5f66d6383b6c9
travishbates <travishbates@gmail.com>
2023-09-30 12:52:06 -0400

    Add TransactionTable with sample transactions from backend.

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = [
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/sample-transactions")
 def hello_world():
-    return {"Hello": "World"}
+    return [
+        {
+            "time": "Time1",
+            "assetBoughtName": "Asset1",
+            "assetBoughtAmount": 123,
+            "assetSoldName": "Asset2",
+            "assetSoldAmount": 456
+        },
+        {
+            "time": "Time2",
+            "assetBoughtName": "Asset2",
+            "assetBoughtAmount": 333,
+            "assetSoldName": "Asset3",
+            "assetSoldAmount": 789
+        }
+    ]
---
+++ gains-tracker-ui/src/TransactionTable.tsx
@@ -0,0 +1,50 @@
+import {TableContainer} from "@mui/material";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableBody from "@mui/material/TableBody";
+import * as React from "react";
+import {useEffect} from "react";
+
+function TransactionTable() {
+    const [transactions, setTransactions] = React.useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8000/sample-transactions")
+            .then(response => response.json())
+            .then((result) => {
+                setTransactions(result);
+            });
+    });
+
+    return (
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Time</TableCell>
+                        <TableCell>Asset Bought</TableCell>
+                        <TableCell>Asset Bought Amount</TableCell>
+                        <TableCell>Asset Sold</TableCell>
+                        <TableCell>Asset Sold Amount</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {transactions.map((transaction) => (
+                        <TableRow key={transaction.time}>
+                            <TableCell>{transaction.time}</TableCell>
+                            <TableCell>{transaction.assetBought}</TableCell>
+                            <TableCell>{transaction.assetBoughtAmount}</TableCell>
+                            <TableCell>{transaction.assetSold}</TableCell>
+                            <TableCell>{transaction.assetSoldAmount}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
+
+export default TransactionTable
