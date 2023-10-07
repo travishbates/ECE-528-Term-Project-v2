3ed58985015690ee9993d3266e19552fa4a9c9cf
travishbates <travishbates@gmail.com>
2023-10-06 22:57:30 -0400

    Dark theme.

--- gains-tracker-ui/src/main.tsx
+++ gains-tracker-ui/src/main.tsx
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
+import createTheme from "@mui/material/styles/createTheme";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import {CssBaseline} from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark'
+    }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <BrowserRouter>
+              <App />
+          </BrowserRouter>
+      </ThemeProvider>
   </React.StrictMode>,
 )
