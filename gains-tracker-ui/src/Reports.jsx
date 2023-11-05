cb5728242e9d15cf3ea9e53840595ea303e4ef40
travishbates <travishbates@gmail.com>
2023-11-05 01:49:12 -0400

    Generate reports asynchronously and upload them to the cloud.

---
+++ gains-tracker-ui/src/Reports.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import ReportTable from "./ReportTable";
+
+function Reports() {
+
+    return (
+        <>
+            <p>Request a report.</p>
+            <ReportTable />
+        </>
+    )
+}
+
+export default Reports
