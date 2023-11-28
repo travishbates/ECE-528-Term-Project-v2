3e6be356558e18ca7c202a852831901c4ae301ee
travishbates <travishbates@gmail.com>
2023-11-28 00:47:27 -0500

    Update allowed origins.

--- backend/main.py
+++ backend/main.py
@@ -91,7 +91,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins = [
-        "*"
+        "http://localhost:5173",
+        "https://gains-tracker-ui-dot-f2023-ece528-bates-travis.ue.r.appspot.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
