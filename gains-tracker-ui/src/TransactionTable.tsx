30b92a46855a897188718afece0acace0d33890f
travishbates <travishbates@gmail.com>
2023-12-03 20:13:58 -0500

    Add spinner when refreshing transactions.

--- gains-tracker-ui/src/TransactionTable.tsx
+++ gains-tracker-ui/src/TransactionTable.tsx
@@ -12,9 +12,13 @@ import Button from "@mui/material/Button";
 import { deleteTransaction, getTransactions } from "./backendService";
 import TablePagination from "@mui/material/TablePagination";
 import TransactionUpload from "./TransactionUpload";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
 const TransactionTable: React.FC = () => {
     const [transactions, setTransactions] = React.useState([]);
+    const [loading, setLoading] = React.useState(true);
     const [count, setCount] = React.useState(0);
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(10);
@@ -40,12 +44,14 @@ const TransactionTable: React.FC = () => {
     }
 
     function refreshTransactions() {
+        setLoading(true);
         getTransactions(page, rowsPerPage)
             .then((result: any) => {
                 setTransactions(result.results);
                 setCount(result.count);
                 setPage(result.page);
                 setRowsPerPage(result.pageSize);
+                setLoading(false);
             });
     }
 
@@ -53,41 +59,51 @@ const TransactionTable: React.FC = () => {
         <>
             <TransactionUpload refreshTransactions={refreshTransactions}></TransactionUpload>
             <h2>Transactions</h2>
-            <TableContainer component={Paper}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Time</TableCell>
-                            <TableCell>Transaction Type</TableCell>
-                            <TableCell>Asset Name</TableCell>
-                            <TableCell>Asset Quantity</TableCell>
-                            <TableCell>Total Asset Amount (USD)</TableCell>
-                            <TableCell>Actions</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {transactions.map((transaction: any) => (
-                            <TableRow key={transaction.id}>
-                                <TableCell>{transaction.time_transacted}</TableCell>
-                                <TableCell>{transaction.transaction_type}</TableCell>
-                                <TableCell>{transaction.asset_name}</TableCell>
-                                <TableCell>{transaction.asset_quantity}</TableCell>
-                                <TableCell>{transaction.total_asset_amount_usd}</TableCell>
-                                <TableCell>
-                                    <Button color="inherit" onClick={() => handleDeleteTransaction(transaction.id)}>Delete</Button>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                count={count}
-                page={page}
-                onPageChange={handlePageChange}
-                rowsPerPage={rowsPerPage}
-                onRowsPerPageChange={handleRowsPerPageChange}
-            />
+
+            { loading ?
+                <Grid container direction={"column"} align="center">
+                    <Grid item>
+                        <CircularProgress />
+                    </Grid>
+                </Grid> :
+                <>
+                    <TableContainer component={Paper}>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Time</TableCell>
+                                    <TableCell>Transaction Type</TableCell>
+                                    <TableCell>Asset Name</TableCell>
+                                    <TableCell>Asset Quantity</TableCell>
+                                    <TableCell>Total Asset Amount (USD)</TableCell>
+                                    <TableCell>Actions</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {transactions.map((transaction: any) => (
+                                    <TableRow key={transaction.id}>
+                                        <TableCell>{transaction.time_transacted}</TableCell>
+                                        <TableCell>{transaction.transaction_type}</TableCell>
+                                        <TableCell>{transaction.asset_name}</TableCell>
+                                        <TableCell>{transaction.asset_quantity}</TableCell>
+                                        <TableCell>{transaction.total_asset_amount_usd}</TableCell>
+                                        <TableCell>
+                                            <Button color="inherit" onClick={() => handleDeleteTransaction(transaction.id)} variant="contained">Delete</Button>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <TablePagination
+                        count={count}
+                        page={page}
+                        onPageChange={handlePageChange}
+                        rowsPerPage={rowsPerPage}
+                        onRowsPerPageChange={handleRowsPerPageChange}
+                    />
+                </>
+            }
         </>
     )
 }
